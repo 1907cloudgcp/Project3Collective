c2e11fa65eb763c46da0fa75ae2f6bd75bec3eac
TheMartyred <bamongeon@gmail.com>
2019-10-03 09:45:10 -0400

    Update build.sh

--- PetInsurance/build.sh
+++ PetInsurance/build.sh
@@ -7,6 +7,7 @@
 #gsutil cp -r dist gs://www.alexandernkingsburyprojects.com/Pet-insurance-Angular
 
 # move into the spring code folder
+# This will need to be changed to reflect the folder containing the spring code
 cd RevaturePetInsurance/
 # compiles the code
 mvn package
@@ -14,10 +15,12 @@ mvn package
 cd ../
 
 #go into the folder
+# This will need to be changed to reflect the folder containing the angular code
 cd Pet-insurance-Angular/
 #sometimes npm doesn't work unless it is freshly installed
 npm install
 #run the anglar
 npm run build
 #put the contents of the anglar into the bucket
+# this will need to be changed to reflect the bucket you intend to store your static web content to
 gsutil cp -r dist/class-example/* gs://www.alexandernkingsburyprojects.com
